d8d45989ad962d3e2fd9ae0270b4a9563a1a5439
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-26 18:17:13 +0200

    feat: add html variables

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
-console.log("test")
+/*html variables*/
+const myForm = document.getElementById("quote-request");
+const userName = document.getElementById("input-name");
+const userSurname = document.getElementById("input-surname");
+const userEmail = document.getElementById("input-email");
+const jobType = document.getElementById("job-type");
+const textArea = document.getElementById("text-area");
+const userPromoCode = document.getElementById("input-promo-code");
+const policyCheck = document.getElementById("policy-check");
