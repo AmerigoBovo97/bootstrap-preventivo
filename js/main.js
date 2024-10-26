94d138cb028971a5e6fc3a55a63c7d702b7f4a26
AmerigoBovo97 <amerigobovo@gamil.com>
2024-10-26 22:24:47 +0200

    feat: add logic variables

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/*html variables*/
+/* html variables */
 const myForm = document.getElementById("quote-request");
 const userName = document.getElementById("input-name");
 const userSurname = document.getElementById("input-surname");
@@ -7,3 +7,33 @@ const jobType = document.getElementById("job-type");
 const textArea = document.getElementById("text-area");
 const userPromoCode = document.getElementById("input-promo-code");
 const policyCheck = document.getElementById("policy-check");
+
+/* logic varaibles */
+const jobs = [ //cost per hour for each job category
+    {
+        jobCategory: "sviluppo backend", 
+        feePerHour: 20.50
+    },
+    {
+        jobCategory: "sviluppo frontend", 
+        feePerHour: 15.30
+    },
+    {
+        jobCategory: "analisi progettuale", 
+        feePerHour: 33.60
+    },
+];
+const promoCodes = [ //discont codes
+    {
+        code: [
+            YHDNU32,
+            JANJC63,
+            PWKCN25,
+            SJDPO96,
+            POCIE24
+        ],
+        discount: 25
+    }
+];
+const hoursNeeded = 10 //hours needed to complete the job
+
